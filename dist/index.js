b80f673a67d2c620baf48a8bef494265ac0bc5d2
bimkon <lexakutsenko@mail.ru>
2020-04-04 20:32:03 +0400

    changed git ignore
    removed dist/

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+import * as $ from 'jquery'
+import Post from './Post'
+//import './styles/styles.css'
+import Waters from './assets/Water.png'
+import './styles/scss.scss'
+
+const post = new Post('Wepback post title', Waters)
+$('pre').html(post.toString())
+
